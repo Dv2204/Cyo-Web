c8fb922406e93a2bb1f6f01725e758fc749dbf62
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-03 01:14:06 +0530

    WIP

--- client/src/components/cards/Cards.js
+++ client/src/components/cards/Cards.js
@@ -4,6 +4,8 @@ import Grid from "@material-ui/core/Grid";
 import { Avatar, Typography, Container} from '@material-ui/core';
 import pic from "../../assets/1.jpg";
 import { useStyles } from "./CardsStyles";
+import ShareIcon from '@material-ui/icons/Share';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 
 const Cards = () => {
     const classes = useStyles();
@@ -24,8 +26,29 @@ const Cards = () => {
                 <Grid container>
                     <Grid item lg={12}>
                         <Typography variant="caption" className={classes.location}>
+                        <LocationOnOutlinedIcon fontSize="small" />
                             blah blah blah, blah city, india
                         </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item lg={12}>
+                        <Typography variant="body2" className={classes.body} >
+                        Lorem ipsum dolor sit amet,
+                         consectetur adipiscing elit.Faucibus auctor eu platea 
+                         dolor etiam vitae aliquam urna.
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item lg={10}>
+                        <Typography variant="body2" className={classes.details}>
+                            View Details
+                            <ShareIcon style={{fontSize: '19px', marginLeft: '1rem', paddingTop: '0.2rem'}}  />
+                        </Typography>
+                    </Grid>
+                    <Grid item lg={2} style={{borderRadius: '50%', backgroundColor: '#E93E7F'}}>
+                    <LocationOnOutlinedIcon className={classes.locationicon} />
                     </Grid>
                 </Grid>
             </Container>
--- client/src/components/cards/CardsStyles.js
+++ client/src/components/cards/CardsStyles.js
@@ -2,7 +2,26 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
     location: {
-        fontSize: '10px',
-        color: 'grey'
+        fontSize: '11px',
+        color: 'grey',
+    },
+    body:{
+        fontSize: '12px'
+    },
+    details: {
+        fontSize: '14px',
+        color: 'grey',
+        padding: '5px',
+    },
+    locationicon: {
+        alignSelf: 'center',
+        fontSize: '31px',
+        justifyContent: 'center',
+        color: '#fff',
+        fontWeight: 'bold',
+        marginLeft: '0.4rem'
     }
+    // icon: {
+
+    // }
 }))
