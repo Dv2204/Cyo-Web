845b583e0c2dd7fd33689ad4b110c58b61491d13
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-05 02:33:16 +0530

    landing page

--- client/src/components/mainPage/Main.js
+++ client/src/components/mainPage/Main.js
@@ -12,7 +12,18 @@ const Main = () => {
         <Grid item lg={12} md={12}>
            <Grid container lg={12} md={12} className={classes.row}>
                <Grid item lg={8} md={8}>
-                    Left
+               <Grid container lg={12} md={12}  justify="center">
+                   <Grid item lg={9} md={9}>
+                   <Paper elevation={3} className={classes.paper} justify="center">
+                       <Typography variant="h2" className={classes.text}>
+                        Starve your <br/>
+                        Distractions <br/>
+                        Feed your <br/>
+                        Focus
+                       </Typography>
+                       </Paper>
+                   </Grid>
+               </Grid>
                </Grid>
                <Grid item lg={4} md={4} >
                <Avatar src={main} alt="Profile" className={classes.image} / >
--- client/src/components/mainPage/MainStyles.js
+++ client/src/components/mainPage/MainStyles.js
@@ -2,15 +2,30 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
     row:{
-        backgroundColor: 'rgba(254, 212, 105, 1)',
+       background: 'linear-gradient(to right bottom, #FED469, #ED0891)',
        // height: '100vh',
         width: '100%',
         overflowX: 'hidden'
     },
     image: {
-        width: theme.spacing(40),
-        height: theme.spacing(80),
-        overflowY: 'hidden',
-       marginTop: '5rem'
+       // width: theme.spacing(40),
+      //  height: theme.spacing(70),
+      height: '100vh',
+      width: '20vw',
+       paddingTop: '3.5rem'
+    },
+    paper:{
+        backgroundColor: 'rgba(255, 255, 255, 0.13)',
+        marginTop: '6rem',
+        height: '65vh',
+        borderRadius: '6%'
+    },
+    text: {
+        fontWeight: 'bold',
+        paddingLeft: '4.8rem',
+        paddingTop: '2.7rem',
+        letterSpacing: '0.1rem',
+        fontSize: '4.2rem'
+        //textAlign: 'center'
     }
 }))
