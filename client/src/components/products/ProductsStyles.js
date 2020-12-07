0bec123ba61022d94bf522775301153e3d798df8
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-08 00:31:16 +0530

    explore products

--- client/src/components/products/ProductsStyles.js
+++ client/src/components/products/ProductsStyles.js
@@ -1,3 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles((theme) => ({ }))
+export const useStyles = makeStyles((theme) => ({
+    heading: {
+        color: '#FFFEFE',
+        letterSpacing: '0.1rem',
+        marginTop: '7rem',
+        fontWeight: 'bold'
+    }
+ }))
