a5735e653aeed1762c82c4e94421ebb030fd504d
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-14 22:40:01 +0530

    WIP

--- client/src/components/connect/ConnectWithUs.js
+++ client/src/components/connect/ConnectWithUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid , Typography, Avatar} from "@material-ui/core";
+import {Grid , Typography, Avatar, Paper} from "@material-ui/core";
 import { useStyles } from "./ConnectWithUsStyles";
 import mobile from "../../assets/mobile.png";
 // import bottom from "../../assets/bottom.png";
@@ -12,8 +12,26 @@ const ConnectWithUs = () => {
             <Grid container lg={12} md={12}>
                 <Grid item lg={7} md={7}>
                     <Grid container justify="center">
-                        <Grid item lg={10} md={10}>
-
+                        <Grid item lg={8} md={8}>
+                            <Paper elevation={0} className={classes.paper}>
+                            <Typography variant="h3" className={classes.heading}>
+                            CONNECT WITH US
+                            </Typography>
+                                <Typography variant="body1" className={classes.body}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Dui ultricies accumsan et viverra. Arcu auctor maecenas 
+                                lectus arcu tortor. Gravida diam sit convallis sagittis. 
+                                Libero tempus phasellus fames nisl in vestibulum. Laoreet 
+                                lacus volutpat, tincidunt maecenas. Scelerisque odio rutrum 
+                                mi netus. Nulla non mauris, adipiscing nisi. Potenti id pretium
+                                 nisl porttitor. Aenean euismod tortor fames odio amet interdum 
+                                 quam et, ornare. Lorem ipsum dolor sit amet, consectetur 
+                                 adipiscing elit. Dui ultricies accumsan et viverra. Arcu auctor
+                                  maecenas lectus arcu tortor. Gravida diam sit convallis sagittis.
+                                   Libero tempus phasellus fames nisl in vestibulum. Aenean euismod
+                                    tortor fames odio amet interdum quam et, ornare.
+                                </Typography>
+                            </Paper>
                         </Grid>
                     </Grid>
                 </Grid>
--- client/src/components/connect/ConnectWithUsStyles.js
+++ client/src/components/connect/ConnectWithUsStyles.js
@@ -1,9 +1,20 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
+    paper:{
+        backgroundColor: 'rgba(248, 248, 248, 1)',
+        marginTop: '10rem'
+    },
+    heading: {
+        fontWeight: 'bold',
+    },
+    body: {
+        marginTop: '0.5rem'
+    },
     image: {
        height: '80vh',
       width: '37vw',
-        paddingTop: '3.1rem'
+        paddingTop: '5rem',
+        paddingBottom: '5rem'
     }
  }))
