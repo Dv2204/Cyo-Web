f6f403329785a2a818f0afd5ec621f6ab35118ac
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-17 14:56:17 +0530

    conditional rendering of gym cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/Routes";
-import history from "./services/History";
-import configureStore from "./redux/store";
-import { ConnectedRouter } from 'connected-react-router'
-import { Provider } from "react-redux";
 
-// const store = configureStore(history)
 const App = () => {
   return (
-    // <Provider store={store}>
     <div className="App">
-    {/* <ConnectedRouter history={history}> */}
         <Routes/>
-      {/* </ConnectedRouter> */}
-      </div>
-   // </Provider>
+    </div>
   );
 };
 
