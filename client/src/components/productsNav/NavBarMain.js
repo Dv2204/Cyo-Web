90e1dca0ac160d661c8a6c2ca378595a0d110b6e
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-18 00:05:01 +0530

    Nav fixed

---
+++ client/src/components/productsNav/NavBarMain.js
@@ -0,0 +1,52 @@
+import React from 'react';
+// import { NavLink as Link} from 'react-router-dom';
+import {Nav, Bars, NavMenu, NavLink, NavDiv, Dot, NavLogo, MobileIcons} from './NavItems';
+import Logo from '../../assets/logo-white.png';
+import '../Nav/navbar.css';
+import {Link} from 'react-scroll';
+// import { Link } from "react-router-dom";
+
+const NavBar = ({toggle}) => {
+    return (
+        <>
+            <Nav style={{ position: 'relative'}}>
+                <MobileIcons onClick={toggle}>
+                    <Bars />
+                </MobileIcons>
+                
+                <NavDiv>
+                    <NavLogo to="/" className="logo-name-img">
+                        <img className="Logo" src={Logo} alt="logo" />
+                        <h2 className="LogoName">Choose Your Option</h2>
+                    </NavLogo>
+                    <NavMenu>
+                        {/* <NavLink activeStyle>
+                            Gyms
+                        </NavLink> */}
+                        <NavLink  to="/gyms" activeStyle>
+                            Gyms
+                        </NavLink>
+                        <Dot />
+                        {/* <Link to="products" smooth={true} duration={2000}><NavLink activeStyle>
+                            Products
+                        </NavLink></Link>
+                        <Dot /> */}
+                        <NavLink to="/compare" activeStyle>
+                            Compare
+                        </NavLink>
+                        <Dot />
+                        <NavLink to="/guide" activeStyle>
+                            Help Centre
+                        </NavLink>
+                        <Dot />
+                        <NavLink  to="/about" activeStyle>
+                            About-Developers
+                        </NavLink>
+                    </NavMenu>
+                </NavDiv>             
+            </Nav>            
+        </>
+    )
+}
+
+export default NavBar;
