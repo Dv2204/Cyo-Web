1c9dc0124e5c732aa341811f56e0964fe76a814d
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-21 21:14:45 +0530

    WIP

--- client/src/Pages/Compare.js
+++ client/src/Pages/Compare.js
@@ -2,14 +2,15 @@ import React from 'react';
 import compare from '../assets/compare.jpg';
 import { useStyles } from "./PageStyles/CompareStyles";
 import NavBar from '../components/aboutNav/NavBarMain';
-import {Grid, Typography} from "@material-ui/core";
+import {Grid, Typography, Chip} from "@material-ui/core";
 import Contact from '../components/contactUs/Contact';
+import CompareSearch from '../components/compareSearch/CompareSearch';
 
 const Compare = () => {
     const classes = useStyles();
         return (
             <>
-            <div style={{backgroundImage: `url(${compare})`, height: '100vh'}}>
+            <div style={{backgroundImage: `url(${compare})`, height: '100vh', width: '100vw'}}>
                 <NavBar/>
                 <div className={classes.compare}>
                 <Grid container lg={12} md={12}>
@@ -19,8 +20,30 @@ const Compare = () => {
                     </Typography>
                 </Grid>
             </Grid>
+             <Grid container lg={12} md={12}>
+                <Grid item lg={12} md={12} >
+                    <Typography variant="h6" className={classes.selection}  justify="center" >
+                       Select the two gyms
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container md={12} lg={12} justify="center" style={{marginTop: '3rem'}}>
+                <Grid item lg={7} md={7} justify="center">
+                    <CompareSearch name="Gym name 1"/>
+                </Grid>
+            </Grid>
+            <Grid container md={12} lg={12} justify="center">
+                <Grid item lg={7} md={7} justify="center">
+                    <CompareSearch name="Gym name 2"/>
+                </Grid>
+            </Grid>
+            <Grid container md={12} lg={12} justify="center">
+                <Grid item lg={2} md={2} justify="center">
+                <Chip className={classes.btn} label="Compare"></Chip>
+                </Grid>
+            </Grid>
             </div>
-            <Contact />
+            {/* <Contact /> */}
             </div>
             </>
         )
--- client/src/Pages/PageStyles/CompareStyles.js
+++ client/src/Pages/PageStyles/CompareStyles.js
@@ -8,8 +8,24 @@ export const useStyles = makeStyles((theme) => ({
         color: 'rgba(251, 91, 33, 1)',
         letterSpacing: '0.1rem',
         textAlign: 'center',
-        boxShadow: "0px 10px 12px rgba(255, 255, 255, 1)",
+        textShadow: "0px 5px 5px rgba(255, 255, 255, 1)",
     },
+    selection: {
+        textAlign: 'center',
+        color: '#ffffff'
+    },
+    btn:{
+        backgroundColor: 'rgba(255, 147, 108, 1)',
+        color: '#fff',
+        marginTop:'0.6rem',
+        padding: '1.3rem 2rem',
+        fontSize: '19px',
+        letterSpacing: '0.1rem',
+        marginTop:'3rem',
+        "&:hover":{
+            cursor: 'pointer'
+        }
+    }
     // compare: {
     //     backgroundImage: '../../assets/compare.jpg'
     // }
