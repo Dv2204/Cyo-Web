ae924d651e8c76acd652c582a425ebb377eed87d
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-21 20:17:33 +0530

    WIP compare page\

--- client/src/Pages/Compare.js
+++ client/src/Pages/Compare.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react';
+import compare from '../assets/compare.jpg';
+import { useStyles } from "./PageStyles/CompareStyles";
+import NavBar from '../components/aboutNav/NavBarMain';
+import {Grid, Typography} from "@material-ui/core";
+import Contact from '../components/contactUs/Contact';
 
-export default class Compare extends Component {
-    render() {
+const Compare = () => {
+    const classes = useStyles();
         return (
-            <div>
-                COMPARE
+            <>
+            <div style={{backgroundImage: `url(${compare})`, height: '100vh'}}>
+                <NavBar/>
+                <div className={classes.compare}>
+                <Grid container lg={12} md={12}>
+                <Grid item lg={12} md={12} >
+                    <Typography variant="h2" className={classes.text}  justify="center" >
+                       COMPARE THE GYMS
+                    </Typography>
+                </Grid>
+            </Grid>
             </div>
+            <Contact />
+            </div>
+            </>
         )
-    }
 }
+
+export default Compare;
+
---
+++ client/src/Pages/PageStyles/CompareStyles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+    text: {
+        paddingTop: '5rem',
+        paddingBottom: '1.7rem',
+        fontWeight: 'bold',
+        color: 'rgba(251, 91, 33, 1)',
+        letterSpacing: '0.1rem',
+        textAlign: 'center',
+        boxShadow: "0px 10px 12px rgba(255, 255, 255, 1)",
+    },
+    // compare: {
+    //     backgroundImage: '../../assets/compare.jpg'
+    // }
+ }))
