6478d4a60cc1f0b08b2afa0ef86873995e89be02
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-21 22:25:29 +0530

    WIP

--- client/src/Pages/MainCompare.js
+++ client/src/Pages/MainCompare.js
@@ -2,7 +2,7 @@ import React from 'react';
 import top from '../assets/about.png';
 import { useStyles } from "./PageStyles/MainCompareStyles";
 import NavBar from '../components/compareNav/NavBarMain';
-import {Grid, Typography} from "@material-ui/core";
+import {Grid, Typography, Chip} from "@material-ui/core";
 import Contact from '../components/contactUs/Contact';
 import Developers from '../components/developers/Developers';
 
@@ -23,13 +23,15 @@ const About = () => {
                    </Grid>
                    <Grid item lg={5} md={5}>
                        <Typography variant="h3">
-                           Gym 1
+                           Gym 2
                        </Typography>
                        <Typography variant="body1" justify="center" className={classes.body}>
                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Faucibus auctor eu platea dolor etiam vitae aliquam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Faucibus auctor eu platea dolor etiam vitae aliquam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Faucibus auctor eu platea dolor etiam vitae aliquam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Faucibus auctor eu platea dolor etiam vitae aliquam urna.
                        </Typography>
                    </Grid>
                </Grid>
+               <hr className={classes.line}/>
+                 <Chip className={classes.btn} label="Trainers"></Chip>
             <Contact />
             </div>
             </>
