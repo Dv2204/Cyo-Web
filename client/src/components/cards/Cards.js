a9f5b6d09d95b6f1b21ddf44a774b3ee769bd4dc
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-29 00:19:24 +0530

    Get gyms done

--- client/src/components/cards/Cards.js
+++ client/src/components/cards/Cards.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import { Avatar, Typography } from "@material-ui/core";
+import { Avatar, Typography, Grid, Paper } from "@material-ui/core";
 import pic from "../../assets/1.jpg";
 import head from "../../assets/2.PNG";
 import { useStyles } from "./CardsStyles";
@@ -11,6 +9,10 @@ import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import StarIcon from "@material-ui/icons/Star";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import {Link} from 'react-router-dom';
+import { ALL_GYMS } from "../../graphql/requests";
+// import { IMAGE_URL } from "../../graphql/requests";
+import { useQuery } from "@apollo/client";
+import Loader from '../Loader';
 
 const Cards = () => {
   const classes = useStyles();
@@ -64,9 +66,26 @@ const Cards = () => {
       location: "Banaras",
     },
   ];
+  const {data:gym, loading, error} = useQuery(ALL_GYMS);
+  if(loading){
+    return (
+      <Grid container lg={12} md={12} justify="center" style={{margin: '5rem'}}>
+        <Grid item lg={3} md={3} justify="center">
+        <Loader color="#fff" />
+        </Grid>
+      </Grid>
+    )
+  }
+
+  if(error){
+    return <p style={{color: '#fff'}}>{error.message}</p>
+  }
+  
+  console.log(gym);
   return (
     <>
-      {GymList.map((item, index) =>
+      {/* {GymList.map((item, index) => */}
+      {gym.gym.map((item, index) =>
         index < 9 ? (
           <Grid item lg={4} md={4} id="gym" className={classes.card} >
             <Paper elevation={4} className={classes.paperNormal}>
@@ -85,18 +104,18 @@ const Cards = () => {
                         <Grid item lg={10} md={10}>
                           <Typography variant="h6" className={classes.title}>
                             {" "}
-                            {item.name}{" "}
+                            {item.gym.name}{" "}
                           </Typography>
                           <Grid container>
                             <Grid item lg={2} md={2}>
                               <Typography className={classes.rating}>
-                                4.3
+                               {item.rating}
                                 <StarIcon className={classes.ratingIcon} />
                               </Typography>
                             </Grid>
                             <Grid item lg={10} md={10}>
                               <Typography className={classes.count}>
-                                2.3k people
+                               {item.gym.counter}
                               </Typography>
                             </Grid>
                           </Grid>
@@ -114,7 +133,7 @@ const Cards = () => {
                 <Grid container justify="center" spacing={0}>
                   <Grid item lg={12} md={12}>
                     <Avatar
-                      src={pic}
+                      src={item.gymimageSet.image}
                       alt="Profile"
                       variant="square"
                       style={{ width: "100%", height: "20vh" }}
@@ -125,16 +144,14 @@ const Cards = () => {
                   <Grid item lg={12} md={12}>
                     <Typography variant="caption" className={classes.location}>
                       <LocationOnOutlinedIcon fontSize="small" />
-                      {item.location}
+                      {item.address}
                     </Typography>
                   </Grid>
                 </Grid>
                 <Grid container className={classes.row}>
                   <Grid item lg={12} md={12}>
                     <Typography variant="body2" className={classes.body}>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit.Faucibus auctor eu platea dolor etiam vitae aliquam
-                      urna.
+                      {item.description}
                     </Typography>
                   </Grid>
                 </Grid>
