fb43c8446f20bb8f1c3afd90e7ee264f8e772e28
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-12-31 14:53:36 +0530

    compare search bars

--- client/src/components/compareSearch/CompareSearchStyles.js
+++ client/src/components/compareSearch/CompareSearchStyles.js
@@ -6,16 +6,31 @@ export const useStyles = makeStyles((theme) => ({
     marginTop: "1rem",
   },
   select: {
-    marginLeft: "-0.5rem",
-    color: "#ffffff",
     backgroundColor: "rgba(255, 190, 157, 1)",
-    borderRadius: "15px",
+    color: "#fff",
+    marginTop: "0.6rem",
+    padding: "1.3rem 1.2rem",
+    fontSize: "19px",
+    letterSpacing: "0.1rem",
+    marginLeft: "-0.5rem",
+    "&:hover": {
+      cursor: "pointer",
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "0.8rem",
+      marginLeft: "3.5rem",
+      marginTop: "1rem",
+      padding: "0.3rem 1.3rem",
+    },
   },
   root: {
     padding: "2px 4px",
     display: "flex",
     alignItems: "center",
     marginLeft: "1rem",
+    [theme.breakpoints.down("xs")]: {
+      width: "94vw",
+    },
   },
   input: {
     marginLeft: theme.spacing(1),
