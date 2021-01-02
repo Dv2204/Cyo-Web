1f2b10837e4a64bf7908616d4313b8be98f29368
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2021-01-02 13:29:46 +0530

    responsive

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,10 +1,27 @@
 import React from "react";
 import BeatLoader from "react-spinners/BeatLoader";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Loader = ({ color }) => (
-  <div className="full-page-loader" style={{ alignSelf: "center" }}>
-    <BeatLoader size={70} color={color} laoading={true} />
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  loadersize: {
+    size: "70",
+    [theme.breakpoints.down("xs")]: {
+      size: "30",
+    },
+  },
+}));
+
+const Loader = ({ color }) => {
+  const classes = useStyles();
+  return (
+    <div className="full-page-loader" style={{ alignSelf: "center" }}>
+      <BeatLoader
+        className={classes.loadersize}
+        color={color}
+        laoading={true}
+      />
+    </div>
+  );
+};
 
 export default Loader;
