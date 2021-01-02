2fba7de3ffc797c93c96f7360b2b6a8450c23d04
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2021-01-02 15:46:53 +0530

    [WIP]

--- client/src/Pages/AboutGyms.js
+++ client/src/Pages/AboutGyms.js
@@ -3,6 +3,7 @@ import { Grid, Typography } from "@material-ui/core";
 import NavBar from "../components/Nav/NavBarMain";
 import Sidebar from "../components/Sidebar/Sidebar";
 import { useStyles } from "./PageStyles/PrivacyPolicyStyles";
+import Contact from "../components/contactUs/Contact";
 
 const AboutGyms = () => {
   const classes = useStyles();
@@ -21,160 +22,99 @@ const AboutGyms = () => {
             <Typography variant="h2" className={classes.text} justify="center">
               About Us
             </Typography>
-            <Typography variant="h2" className={classes.name} justify="center">
+            <Typography variant="h3" className={classes.name} justify="center">
               Choose Your Option Pvt. Ltd.
             </Typography>
           </Grid>
         </Grid>
         <Grid container lg={12} md={12} xs={12} sm={12} justify="center">
           <Grid item lg={8} sm={10} xs={10} md={8} justify="center">
-            <Typography variant="h4" className={classes.textheading}>
-              Personal Information -
-            </Typography>
-            <Typography variant="body1" className={classes.textbody}>
-              Choose Your Option Pvt. Ltd. is the licensed owner of the brand
-              CYOGYMS and the website cyogyms.com (”The Site”) from Choose Your
-              Option Pvt. Ltd. respects your privacy. This Privacy Policy
-              provides succinctly the manner your data is collected and used by
-              cyogyms.com on the Site. As a visitor to the Site/ Customer you
-              are advised to please read the Privacy Policy carefully. By
-              accessing the services provided by the Site you agree to the
-              collection and use of your data by Choose Your Option Pvt. Ltd. in
-              the manner provided in this Privacy Policy.
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container lg={12} md={12} xs={12} sm={12} justify="center">
-          <Grid item lg={8} sm={10} xs={10} md={8} justify="center">
-            <Typography variant="h4" className={classes.textheading}>
-              What information is, or may be, collected form you?
-            </Typography>
-            <Typography variant="body1" className={classes.textbody}>
-              As part of the registration process on the Site, cyogyms.com may
-              collect the following personally identifiable information about
-              you: Name including first and last name, alternate email address,
-              mobile phone number and contact details, Postal code, Demographic
-              profile (like your age, gender, occupation, education, address
-              etc.) and information about the pages on the site you
-              visit/access, the links you click on the site, the number of times
-              you access the page and any such browsing information.
+            <Typography
+              variant="h6"
+              className={classes.textbody}
+              style={{ color: "#494848" }}
+            >
+              CYO GYMS introduced in 2020-2021 when Mr. Harish Saini and Vikas
+              Malik realized that users didn’t want to or couldn’t pay yearly
+              gym membership fees upfront. There was intent since everyone tried
+              several fitness centers and local parks, but it wasn’t practical
+              for long. It was then that a simple idea was worked upon – working
+              out should work out. What started with a handful of gyms in Rewari
+              is now offering fitness enthusiasts across India the flexibility
+              to workout whenever, wherever, and however they please. We have
+              successfully digitized the Fitness Industry and created an
+              interconnected community of fitness centers / gyms and fitness
+              enthusiasts. Our new age functions have transformed the fitness
+              ecosystem. CYO GYMS is an alias for comprehensive fitness
+              solutions. We are successfully removing friction and creating
+              marketplace efficiencies through technology and bringing in the
+              age of Fittech in India.
             </Typography>
           </Grid>
         </Grid>
         <Grid container lg={12} md={12} xs={12} sm={12} justify="center">
           <Grid item lg={8} sm={10} xs={10} md={8} justify="center">
             <Typography variant="h4" className={classes.textheading}>
-              How do we Collect the Information ?
+              Our Motto
             </Typography>
-            <Typography variant="body1" className={classes.textbody}>
-              cyogyms.com will collect personally identifiable information about
-              you only as part of a voluntary registration process, on-line
-              survey or any combination thereof. The Site may contain links to
-              other Web sites. cyogyms.com is not responsible for the privacy
-              practices of such Web sites which it does not own, manage or
-              control. The Site and third-party vendors, including Google, use
-              first-party cookies (such as the Google Analytics cookie) and
-              third-party cookies (such as the DoubleClick cookie) together to
-              inform, optimize, and serve ads based on someone’s past visits to
-              the Site.
+            <Typography
+              variant="h6"
+              className={classes.textbody}
+              style={{ color: "#494848" }}
+            >
+              Inspire people to include fitness in their daily routine by
+              creating a seamless network of gyms and fitness studios. Fitness
+              is a basic of life that shouldn’t be expensive or inaccessible.
             </Typography>
           </Grid>
         </Grid>
         <Grid container lg={12} md={12} xs={12} sm={12} justify="center">
           <Grid item lg={8} sm={10} xs={10} md={8} justify="center">
             <Typography variant="h4" className={classes.textheading}>
-              How is information used ?
+              Our Vision
             </Typography>
-            <Typography variant="body1" className={classes.textbody}>
-              cyogyms.com will use your personal information to provide
-              personalized features to you on the Site and to provide for
-              promotional offers to you through the Site and other channels.
-              cyogyms.com will also provide this information to its business
-              associates and partners to get in touch with you when necessary to
-              provide the services requested by you. cyogyms.com will use this
-              information to preserve transaction history as governed by
-              existing law or policy. cyogyms.com may also use contact
-              information internally to direct its efforts for product
-              improvement, to contact you as a survey respondent, to notify you
-              if you win any contest; and to send you promotional materials from
-              its contest sponsors or advertisers. cyogyms.com will also use
-              this information to serve various promotional and advertising
-              materials to you via display advertisements through the Google Ad
-              network on third party websites. You can opt out of Google
-              Analytics for Display Advertising and customize Google Display
-              network ads using the Ads Preferences Manager. Information about
-              Customers on an aggregate (exlcuding any information that may
-              identify you specifically) covering Customer transaction data and
-              Customer demographic and location data may be provided to partners
-              of cyogyms.com for the purpose of creating additional features on
-              the website, creating appropriate merchandising or creating new
-              products and services and conducting marketing research and
-              statistical analysis of customer behaviour and transactions.
+            <Typography
+              variant="h6"
+              className={classes.textbody}
+              style={{ color: "#494848" }}
+            >
+              Economical, convenient, and non-restrictive fitness solutions to
+              create a nexus of happy and healthy people.
             </Typography>
           </Grid>
         </Grid>
         <Grid container lg={12} md={12} xs={12} sm={12} justify="center">
           <Grid item lg={8} sm={10} xs={10} md={8} justify="center">
             <Typography variant="h4" className={classes.textheading}>
-              With whom your information will be shared -
+              For our Users
             </Typography>
-            <Typography variant="body1" className={classes.textbody}>
-              cyogyms.com will not use your financial information for any
-              purpose other than to complete a transaction with you. cyogyms.com
-              does not rent, sell or share your personal information and will
-              not disclose any of your personally identifiable information to
-              third parties. In cases where it has your permission to provide
-              products or services you’ve requested and such information is
-              necessary to provide these products or services the information
-              may be shared with cyogyms.com business associates and partners.
-              cyogyms.com may, however, share consumer information on an
-              aggregate with its partners or third parties where it deems
-              necessary. In addition cyogyms.com may use this information for
-              promotional offers, to help investigate, prevent or take action
-              regarding unlawful and illegal activities, suspected fraud,
-              potential threat to the safety or security of any person,
-              violations of the Site’s terms of use or to defend against legal
-              claims; special circumstances such as compliance with subpoenas,
-              court orders, requests/order from legal authorities or law
-              enforcement agencies requiring such disclosure.
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container lg={12} md={12} xs={12} sm={12} justify="center">
-          <Grid item lg={8} sm={10} xs={10} md={8} justify="center">
-            <Typography variant="h4" className={classes.textheading}>
-              What Choice are available to you regarding collection, use and
-              distribution of your information ?
-            </Typography>
-            <Typography variant="body1" className={classes.textbodylast}>
-              To protect against the loss, misuse and alteration of the
-              information under its control, cyogyms.com has in place
-              appropriate physical, electronic and managerial procedures. For
-              example, cyogyms.com servers are accessible only to authorized
-              personnel and your information is shared with employees and
-              authorised personnel on a need to know basis to complete the
-              transaction and to provide the services requested by you. Although
-              cyogyms.com will endeavour to safeguard the confidentiality of
-              your personally identifiable information, transmissions made by
-              means of the Internet cannot be made absolutely secure. By using
-              this site, you agree that cyogyms.com will have no liability for
-              disclosure of your information due to errors in transmission or
-              unauthorized acts of third parties.
+            <Typography
+              variant="h6"
+              className={classes.textbody}
+              style={{ color: "#494848" }}
+            >
+              CYO GYMS dynamic approach will help user to stay fit & motivated.
+              we have been breaking barriers – prices, fixed schedules, and
+              boring routines , now you can workout when you want. CYO GYMS is
+              where you start achieving your fitness goals!
+              <br />
+              <br />
+              <Typography
+                style={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  marginBottom: "4rem",
+                }}
+                className={classes.textbody}
+                variant="h5"
+              >
+                “ All progress takes place outside the comfort zone ”
+              </Typography>
             </Typography>
           </Grid>
         </Grid>
-        <Grid
-          container
-          lg={12}
-          md={12}
-          justify="center"
-          style={{ backgroundColor: "#282828", height: "8vh" }}
-        >
-          <Grid item lg={8} md={8} sm={10} xs={10} justify="center">
-            <Typography variant="h6" className={classes.link}>
-              Choose Your Option &copy; 2020
-            </Typography>
-          </Grid>
+        <Grid container>
+          <Contact />
         </Grid>
       </div>
     </>
