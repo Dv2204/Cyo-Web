f90fdc6568561e697606786f825f8c7ce093e6c9
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2021-01-06 14:35:34 +0530

    done

--- client/src/components/reviewCard/ReviewCardStyles.js
+++ client/src/components/reviewCard/ReviewCardStyles.js
@@ -4,15 +4,11 @@ export const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: "2rem",
     backgroundColor: "rgba(240, 240, 240, 1)",
-    // height: "20vh",
     borderRadius: "20px",
     [theme.breakpoints.down("xs")]: {
       margin: "4rem 1rem",
     },
   },
-  // text: {
-  //   height: "5vh",
-  // },
   cirpaper: {
     height: "13vh",
     width: "13vh",
@@ -20,6 +16,10 @@ export const useStyles = makeStyles((theme) => ({
     backgroundColor: "rgba(196, 196, 196, 1)",
     marginTop: "-1.5rem",
     marginLeft: "-1rem",
+    [theme.breakpoints.down("xs")]: {
+      height: "10vh",
+      width: "10vh",
+    },
   },
   images: {
     height: "13vh",
@@ -27,6 +27,10 @@ export const useStyles = makeStyles((theme) => ({
     borderRadius: "50%",
     marginTop: "-1rem",
     backgroundColor: "rgba(196, 196, 196, 1)",
+    [theme.breakpoints.down("xs")]: {
+      height: "10vh",
+      width: "10vh",
+    },
   },
   name: {
     marginLeft: "1.5rem",
@@ -34,6 +38,11 @@ export const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down("xs")]: {
       fontSize: "1.2rem",
       marginLeft: "1rem",
+    },
+  },
+  text: {
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "0.8rem",
     },
   },
 }));
