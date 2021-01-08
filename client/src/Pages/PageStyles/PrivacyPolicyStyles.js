09a1351bb991d2c2656e21388ded2ecc79e85685
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2021-01-08 21:00:40 +0530

    thode se changes

--- client/src/Pages/PageStyles/PrivacyPolicyStyles.js
+++ client/src/Pages/PageStyles/PrivacyPolicyStyles.js
@@ -5,7 +5,8 @@ export const useStyles = makeStyles((theme) => ({
     paddingTop: "5rem",
     paddingBottom: "1.4rem",
     fontWeight: "bold",
-    color: "rgba(233, 62, 127, 1)",
+    color: "#FB5B21",
+    // textGradient: "linear-gradient(to right bottom, #FB5B21, #000)",
     letterSpacing: "0.1rem",
     textAlign: "center",
     [theme.breakpoints.down("xs")]: {
@@ -21,7 +22,7 @@ export const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     [theme.breakpoints.down("xs")]: {
       fontSize: "1.2rem",
-      padding: "1rem 2rem"
+      padding: "1rem 2rem",
     },
   },
   textheading: {
