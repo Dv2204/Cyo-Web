4fd855363ce5447d6210ce8e7cfc301e33170967
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2021-01-13 16:44:03 +0530

    WIP responsiveness

--- client/src/components/contactUs/ContactUsNewStyles.js
+++ client/src/components/contactUs/ContactUsNewStyles.js
@@ -6,58 +6,101 @@ export const useStyles = makeStyles((theme) => ({
     fontWeight: "bold",
     marginLeft: "2rem",
     fontSize: "2.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1.7rem",
+      textAlign: "center",
+    },
   },
   headingtwo: {
     color: "#fff",
     fontWeight: "bold",
     marginTop: "0.5rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      fontWeight: "normal",
+      textAlign: "center",
+      marginTop: "1rem",
+    },
   },
   headingbutton: {
     backgroundColor: "#fff",
     padding: "0.5rem 2.3rem",
     textAlign: "center",
+    [theme.breakpoints.down("xs")]: {
+      padding: "0.2rem 1.5rem",
+      marginTop: "1.3rem",
+    },
   },
   headingbuttontext: {
     textAlign: "center",
     color: "#000",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "0.5rem",
+    },
   },
   download: {
     color: "#fff",
     marginTop: "2.5rem",
     marginLeft: "12rem",
     marginBottom: "1rem",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: "1rem",
+      fontSize: "1.2rem",
+    },
   },
   mac: {
     width: "10vw !important",
     marginLeft: "5vw",
     padding: "12px !important",
     [theme.breakpoints.down("xs")]: {
-      width: "20vw !important",
-      padding: "8px 12vw !important",
+      width: "24vw !important",
+      marginLeft: "-3rem",
     },
   },
   google: {
     width: "12vw !important",
     marginLeft: "5vw",
     [theme.breakpoints.down("xs")]: {
-      width: "24vw !important",
+      width: "25vw !important",
+      marginTop: "0.4rem",
     },
   },
   divider: {
     backgroundColor: "#fff",
     marginTop: "1.5rem",
     height: "29vh",
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
   },
   pagestop: {
     color: "#fff",
     marginTop: "2.5rem",
     marginLeft: "4rem",
     marginBottom: "0.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginTop: "1.2rem",
+      marginLeft: "9rem",
+    },
   },
   pages: {
     color: "#fff",
     marginLeft: "4rem",
     marginBottom: "0.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginLeft: "6.96rem",
+    },
+  },
+  pagessupport: {
+    color: "#fff",
+    marginLeft: "4rem",
+    marginBottom: "0.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginLeft: "9rem",
+    },
   },
   pagestwotop: {
     color: "#fff",
@@ -65,23 +108,42 @@ export const useStyles = makeStyles((theme) => ({
     marginLeft: "2rem",
     marginRight: "3rem",
     marginBottom: "0.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginTop: "0.1rem",
+      marginLeft: "6rem",
+    },
   },
   pagestwo: {
     color: "#fff",
     marginLeft: "2rem",
     marginRight: "3rem",
     marginBottom: "0.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginTop: "0.1rem",
+      marginLeft: "6rem",
+    },
   },
   pagesthreetop: {
     color: "#fff",
     marginTop: "2.5rem",
     marginLeft: "1.5rem",
     marginBottom: "0.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginTop: "0.1rem",
+      marginLeft: "8rem",
+    },
   },
   pagesthree: {
     color: "#fff",
     marginLeft: "1.5rem",
     marginBottom: "0.4rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      marginLeft: "8.9rem",
+    },
   },
   paper: {
     borderRadius: "20px",
@@ -91,6 +153,9 @@ export const useStyles = makeStyles((theme) => ({
   paperhead: {
     marginTop: "1.5rem",
     textAlign: "center",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+    },
   },
   dividersec: {
     marginTop: "2.5rem",
@@ -98,6 +163,9 @@ export const useStyles = makeStyles((theme) => ({
     backgroundColor: "#fff",
     height: "54vh",
     marginBottom: "2.5rem",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "none",
+    },
   },
   icons: {
     color: "#fff",
